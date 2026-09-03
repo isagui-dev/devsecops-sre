ec27a7da34d13d92c6ba0da327764312fd433cd6
isagui-dev <nicolasprietogomez24@gmail.com>
2026-09-03 10:30:36 -0500

    fix(security): ignore B104 bandit warning for docker host binding

--- app/app.py
+++ app/app.py
@@ -20,4 +20,4 @@ def metrics():
     return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)  # nosec B104
