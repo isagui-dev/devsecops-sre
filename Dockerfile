a16774b65b5d0042bebf584e02c60764d4ce6edc
isagui-dev <nicolasprietogomez24@gmail.com>
2026-09-03 19:04:46 -0500

    fix: fixed vulnerabilities

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8
+FROM python
 
 RUN apt-get update && apt-get upgrade -y
 WORKDIR /home/app
--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
         conn = pymysql.connect(
             host='db',
             user=os.getenv('MYSQL_USER'),
-            password='MYSQL_PASSWORD',
+            password=os.getenv('MYSQL_PASSWORD'),
             database=os.getenv('MYSQL_DATABASE')
         )
         conn.close()
